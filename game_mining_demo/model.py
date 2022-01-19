762c2bc6c3b9112f3a0bf056fa8aaf61833c859a
Danillo Lessa Bernardineli <danilo.lessa@gmail.com>
2022-01-19 15:49:43 -0300

    working demo

--- game_mining_demo/model.py
+++ game_mining_demo/model.py
@@ -18,26 +18,45 @@ class Player():
     player_no: int
 
     def likelihood(self,
-                   actions: dict[int, dict[int, int]]):
-        alpha_weight = 5
-        beta_weight = 2
-        gamma_weight = 1
-        total_weight = alpha_weight + beta_weight + gamma_weight
-        alpha = alpha_weight / total_weight
-        beta = beta_weight / total_weight
-        gamma = gamma_weight / total_weight
+                   actions: list[int]):
+        """
+        
+        actions: {time_no: action_no}
+        """
 
-        non_player_actions = {k: v
-                              for k, v
-                              in actions.items()
-                              if k != self.player_no}
+        if len(actions) > 1:
+            alpha_weight = 5
+            beta_weight = 2
+            gamma_weight = 1
+            total_weight = alpha_weight + beta_weight + gamma_weight
+            alpha = alpha_weight / total_weight
+            beta = beta_weight / total_weight
+            gamma = gamma_weight / total_weight
 
-        # TODO
-        proportional = 0.0
-        integral = proportional
-        derivative = proportional
+            N_i = 10
+            N_b = 5
 
-        return alpha * proportional + beta * integral + gamma * derivative
+            def estimator(lst):
+                return sum(lst) / len(lst)
+
+            proportional = estimator(actions)
+
+            past_estimators = []
+            if len(actions) > (N_i + N_b + 2):
+                for i in range(N_b):
+                    n = len(actions)
+                    eff_actions = actions[n-(N_i + i):(n - i)]
+                    past_estimators.append(estimator(eff_actions))
+                integral = sum(past_estimators) / len(past_estimators)
+                derivative = (past_estimators[0] - past_estimators[1]) / 2
+            else:
+                integral = proportional
+                derivative = proportional
+
+
+            return alpha * proportional + beta * integral + gamma * derivative
+        else:
+            return 1 / 2
 
 
 # %%
@@ -55,6 +74,7 @@ PARAMS: dict = {
 INITIAL_STATE: dict = {
     'players': INITIAL_PLAYERS,
     'actions': {},
+    'past_actions': {0: [], 1: []},
     'payoffs': {},
     'payoff_tensor': PAYOFF_TENSOR
 }
@@ -62,27 +82,44 @@ INITIAL_STATE: dict = {
 
 def s_actions(params, _2, _3, state, _5):
 
+    past_actions = state['past_actions']
     players: list[Player] = state['players']
     payoff_tensor: list[list[list[int]]] = state['payoff_tensor']
 
     choices = {}
+
     for i, player in enumerate(players):
-        payoff = payoff_tensor[i]
-        p_a1 = player.likelihood(state['actions'])
-        p_a2 = 1 - p_a1
 
-        payoff_1_estimator = p_a1 * payoff[0][0] + p_a2 * payoff[0][1]
-        payoff_2_estimator = p_a1 * payoff[1][0] + p_a2 * payoff[1][1]
-        # payoff_avg_estimator = (payoff_1_estimator + payoff_2_estimator) / 2
-        # payoff_min = min(payoff_1_estimator, payoff_2_estimator)
-        # payoff_max = max(payoff_1_estimator, payoff_2_estimator)
-        utility_1 = payoff_1_estimator
-        utility_2 = payoff_2_estimator
-        total_utility = sum([utility_1, utility_2])
+        if player.player_no == 0:
+            other_player_actions = past_actions[1]
+        elif player.player_no == 1:
+            other_player_actions = past_actions[0]
+        else:
+            raise Exception()
 
-        probability_1 = utility_1 / total_utility
-        choice = (random() > probability_1)
-        choices[i] = choice
+        p_1 = player.likelihood(other_player_actions)
+
+        # Payoff matrix and probability vector
+        R = payoff_tensor[i]
+        P = (p_1, 1 - p_1)
+
+        # Compute expected payoffs
+        if player.player_no == 0:
+            pi_1 = R[0][0] * P[0] + R[0][1] * P[1]
+            pi_2 = R[1][0] * P[0] + R[1][1] * P[1]
+        elif player.player_no == 1:
+            pi_1 = R[0][0] * P[0] + R[1][0] * P[1]
+            pi_2 = R[0][1] * P[0] + R[1][1] * P[1]
+        else:
+            pass
+    
+        # Iverson Bracket
+        if pi_1 > pi_2:
+            choice = 1
+        else:
+            choice = 0
+
+        choices[player.player_no] = choice
 
     return ('actions', choices)
 
@@ -107,7 +144,7 @@ def s_players(params, _2, _3, state, _5):
 def s_payoff_tensor(params, _2, _3, state, _5):
 
     T = np.array(state['payoff_tensor'])
-    if state['timestep'] > 100:
+    if state['timestep'] > 10:
         
         sigma = np.random.randn(2, 2, 2)
         new_T = T + sigma
@@ -115,6 +152,14 @@ def s_payoff_tensor(params, _2, _3, state, _5):
     else:
         return ('payoff_tensor', T.tolist())
 
+
+def s_past_actions(_1, _2, _3, state, _5):
+    h = state['past_actions'].copy()
+    a = state['actions']
+
+    h[0].append(a[0])
+    h[1].append(a[1])
+    return ('past_actions', h)
 
 
 BLOCKS: list[dict] = [
@@ -125,6 +170,15 @@ BLOCKS: list[dict] = [
         },
         'variables': {
             'actions': s_actions
+        }
+    },
+    {
+        'name': 'Past Actions',
+        'policies': {
+
+        },
+        'variables': {
+            'past_actions': s_past_actions
         }
     },
     {
